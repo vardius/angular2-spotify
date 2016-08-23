0616c6fd7ca56f3d45b5cd9fdddb37e33626ba86
Rafał Lorenz <vardius@gmail.com>
2016-08-23 15:29:49 +0200

    small upgrades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {SpotifyService} from './services/spotify.service';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
     directives: [NavbarComponent],
     providers: [HTTP_PROVIDERS, SpotifyService]
 })
--- src/app/models/album.spec.ts
+++ src/app/models/album.spec.ts
@@ -2,7 +2,7 @@ import {Album} from "./Album";
 
 describe('Album', () => {
     it('has id', () => {
-        let album:Album = {id: 1, name: 'what ever'};
+        let album:Album = {id: 1};
         expect(album.id).toEqual(1);
     });
 });
